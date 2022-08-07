5fc4faf559bc374801753de0aab2fe09770fc2a6
Ridho Rakhaman <91518402+bapongka@users.noreply.github.com>
2022-08-07 20:42:25 +0700

    Update Footer
    Add Link from next/link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,42 +1,45 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="container text-white p-3">
       <h1 className="font-bold text-lg pb-5 text-center">
-        <a href="/">
-          FAST NEWS <sup>88</sup>
-        </a>
+        <Link href="/">
+          <a>
+            FAST NEWS <sup>88</sup>
+          </a>
+        </Link>
       </h1>
       <div className="flex pb-5 justify-center">
         <p className="font-normal text-sm px-5 border-l border-button">
-          <a href="#" className="hover:text-button">
-            About Us
-          </a>
+          <Link href="#">
+            <a className="hover:text-button">About Us</a>
+          </Link>
         </p>
         <p className="font-normal text-sm px-5 border-l border-button">
-          <a href="#" className="hover:text-button">
-            Redaksi
-          </a>
+          <Link href="#">
+            <a className="hover:text-button">Redaksi</a>
+          </Link>
         </p>
         <p className="font-normal text-sm px-5 border-x border-button">
-          <a href="#" className="hover:text-button">
-            Pedoman Media Siber
-          </a>
+          <Link href="#">
+            <a className="hover:text-button">Pedoman Media Siber</a>
+          </Link>
         </p>
         <p className="font-normal text-sm px-5 border-r border-button">
-          <a href="#" className="hover:text-button">
-            Career
-          </a>
+          <Link href="#">
+            <a className="hover:text-button">Career</a>
+          </Link>
         </p>
         <p className="font-normal text-sm px-5 border-r border-button">
-          <a href="#" className="hover:text-button">
-            Disclaimer
-          </a>
+          <Link href="#">
+            <a className="hover:text-button">Disclaimer</a>
+          </Link>
         </p>
       </div>
-      <div class="w-full pt-3 border-t">
-        <p class="font-medium text-xs text-secondary text-center">2022 &copy; Ridho Rakhaman. All rights reserved</p>
+      <div className="w-full pt-3 border-t">
+        <p className="font-medium text-xs text-secondary text-center">2022 &copy; Ridho Rakhaman. All rights reserved</p>
       </div>
     </div>
   );
